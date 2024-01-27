dddb5869bb13451937befc0f9d679349a5f2ff3f
jsy8315 <tdhitbtd@gmail.com>
2024-01-27 13:41:57 +0900

    섹션02 코드 구현능력 기르기_자릿수의 합_강의_정수를 str로 처리해서 자리수 합 구하기

--- 02_06_sumOfDigits_lec.py
+++ 02_06_sumOfDigits_lec.py
@@ -2,6 +2,8 @@
 #섹션2. 코드 구현능력 기르기
 #자릿수의 합, 강의 내용
 
+#몫과 나머지로 처리하기
+'''
 import sys
 
 n = int(sys.stdin.readline())
@@ -12,6 +14,29 @@ def digit_sum(x):
     while x > 0:
         sum += x %10
         x = x // 10
+    return sum
+
+max = -2147000000
+
+for x in a:
+    tot = digit_sum(x)
+    if tot > max:
+        max = tot
+        res = x
+
+print(res)
+'''
+
+#정수를 str처리해서 자리수 합 구해보기, 좀더 파이썬처럼 사용하기
+import sys
+
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+
+def digit_sum(x):
+    sum = 0
+    for i in str(x):
+        sum += int(i)
     return sum
 
 max = -2147000000
