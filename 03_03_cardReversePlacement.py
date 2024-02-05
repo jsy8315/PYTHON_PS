1dbcea25e14e996a48b0b7d45eb81b7db0622435
jsy8315 <tdhitbtd@gmail.com>
2024-02-06 06:32:27 +0900

    섹션3. 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색)_03.카드 역배치(정올 기출)

---
+++ 03_03_cardReversePlacement.py
@@ -0,0 +1,21 @@
+#자료구조와 알고리즘 입문
+#섹션3. 탐색 & 시뮬레이션(string, 1차원, 2차원 리스트 탐색)
+#03.카드 역배치(정올 기출)
+import sys
+
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+for i in range(10):
+    ai, bi = map(int, sys.stdin.readline().split())
+    B = A
+    print(B)
+    for j in range(ai - 1):
+        A[j] = B[j]
+    for j in range(ai - 1, bi):
+        A[j] = B[ai + bi - 2 - j]
+    for j in range(bi):
+        A[j] = B[j]
+    B = A
+
+for i in range(20):
+    print(A[i], end = ' ')
