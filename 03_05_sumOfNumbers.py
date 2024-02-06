af24ad22d0e6e7c62a1b59ada1b1f1497e295681
jsy8315 <tdhitbtd@gmail.com>
2024-02-07 05:34:52 +0900

    섹션3. 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색)_05. 수들의 합

--- 03_05_sumOfNumbers.py
+++ 03_05_sumOfNumbers.py
@@ -2,3 +2,20 @@
 #섹션3. 탐색 & 시뮬레이션(string, 1차원, 2차원 리스트 탐색)
 #05. 수들의 합
 
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+A = list(map(int, sys.stdin.readline().split()))
+
+sum = [0] * N
+cnt = 0
+
+for i in range(N):
+    for j in range(i, N):
+        sum[i] = sum[i] + A[j]
+        if sum[i] == M:
+            cnt += 1
+            break
+
+print(cnt)
